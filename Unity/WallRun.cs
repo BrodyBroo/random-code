24b590cf60a70bb2153ab10a53bf4db10536c212
BrodyBroo <149398055+BrodyBroo@users.noreply.github.com>
2024-05-21 11:32:09 -0400

    Create WallRun.cs

---
+++ Unity/WallRun.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class WallRun : MonoBehaviour {
+  public float wallDistance = 0.5f;
+  public float wallRunGravity = 1f;
+  public float wallRunJuumpForce = 10f;
+  public float wallRunSpeed = 5f;
+  public float wallRunDuration = 2f;
+  public LayerMask wallLayer;
+
+  private bool isWallRunning = false;
+  private bool canWallRun = false;
+  private float wallRunStartTime;
+
+  private Rigidbody rb;
+
+  void Start() {
+    rb = GetComponent<Rigidbody>)();
+  }
+
+  void Update() {
+    CheckForWall();
+    WallRunControl();
+  }
+
+  void CheckForWall() {
+    RaycastHit hit;
+    if(Physics.Raycast(transform.position, transform.right, out hit, wallDistance, wallLayer) || Physics.Raycast(transform.position, -transform.right, out hit, wallDistance, wallLayer()) {
+      canWallRun = True;
+    }
+    else {
+      canWallRun = false;
+  }
+}
+
+void WallRunControl() {
+  if(canWallRun && Input.GetKeyDown(KeyCode.Space)) {
+    StartWallRun();
+  }
+
+  if(isWallRunning) {
+    rb.AddForce(Vector3.down * wallRunGravity, ForceMode.Acceleration);
+
+    rb.velocity = transform.forward * wallRunSpeed;
+
+    if(Time.time - wallRunStartTime >= wallRunDuration) {
+      StopWallRun();
+    }
+
+    if(Input.GetKeyDown(KeyCode.
+  }
+}
